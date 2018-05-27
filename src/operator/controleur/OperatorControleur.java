47638fc8ee41c688874b1b128d9e2d706339784f
Thoma <Thoma@DESKTOP-TMEKRMR>
2018-05-27 19:01:38 +0200

    doc controleur

--- src/operator/controleur/OperatorControleur.java
+++ src/operator/controleur/OperatorControleur.java
@@ -8,10 +8,8 @@ package operator.controleur;
 import operator.modele.OperatorModele;
 import operator.vue.OperatorVue;
 import java.util.*;
-import operator.modele.Aeroport;
-import operator.modele.Bateau;
-import operator.modele.Port;
-import operator.modele.Vol;
+import operator.modele.*;
+
 
 /**
  *
@@ -22,15 +20,26 @@ public class OperatorControleur {
     private OperatorModele om = null;
     private OperatorVue ov = null;
 
+    /**
+     * Constructeur par défaut
+     */
     public OperatorControleur() {
-        //constructeur par défaut
+
     }
 
+    /**
+     * Constructeur complet
+     * @param om classe modele
+     * @param ov class vue
+     */
     public OperatorControleur(OperatorModele om, OperatorVue ov) {
         this.om = om;
         this.ov = ov;
     }
 
+    /**
+     * Gestion du menu des sous-menus
+     */
     public void gestion() {
         int choix;
         do {
@@ -60,30 +69,33 @@ public class OperatorControleur {
         } while (choix != 6);
     }
 
+    /**
+     * Gestion du menu des bateaux
+     */
     public void gestionBateau() {
         int choix;
         do {
             choix = ov.menuBateau();
             switch (choix) {
-                case 1:
+                case 1://ajouter
                     ajoutBateau();
                     break;
-                case 2:
+                case 2://affilier un port
                     affilierPort();
                     break;
-                case 3:
+                case 3://afficher
                     listeBateaux();
                     break;
-                case 4:
+                case 4://recherche spécifique
                     rechBateau();
                     break;
-                case 5:
+                case 5://modifier le supplément single
                     modifSupSingle();
                     break;
-                case 6:
+                case 6://supprimer
                     suppBateau();
                     break;
-                case 7:
+                case 7://retour
                     ov.affMsg("Retour au menu principal !");
                     ov.menu();
                     break;
@@ -93,24 +105,27 @@ public class OperatorControleur {
         } while (choix != 7);
     }
 
+    /**
+     * Gestion du menu des ports
+     */
     public void gestionPort() {
         int choix;
         do {
             choix = ov.menuPort();
             switch (choix) {
-                case 1:
+                case 1://ajouter
                     ajoutPort();
                     break;
-                case 2:
+                case 2://afficher
                     listePorts();
                     break;
-                case 3:
+                case 3://rechercher
                     rechPort();
                     break;
-                case 4:
+                case 4://supprimer
                     suppPort();
                     break;
-                case 5:
+                case 5://retour
                     ov.affMsg("Retour au menu principal !");
                     ov.menu();
                     break;
@@ -120,27 +135,30 @@ public class OperatorControleur {
         } while (choix != 5);
     }
 
+    /**
+     * Gestion du menu des vols
+     */
     public void gestionVol() {
         int choix;
         do {
             choix = ov.menuVol();
             switch (choix) {
-                case 1:
+                case 1://ajouter
                     ajoutVol();
                     break;
-                case 2:
+                case 2://affilier un aéroport
                     affilierAeroport();
                     break;
-                case 3:
+                case 3://afficher
                     listeVols();
                     break;
-                case 4:
+                case 4://rechercher
                     rechVol();
                     break;
-                case 5:
+                case 5://supprimer
                     suppVol();
                     break;
-                case 6:
+                case 6://retour
                     ov.affMsg("Retour au menu principal !");
                     ov.menu();
                     break;
@@ -150,24 +168,27 @@ public class OperatorControleur {
         } while (choix != 6);
     }
 
+    /**
+     * Gestion du menu des aéroports
+     */
     public void gestionAeroport() {
         int choix;
         do {
             choix = ov.menuAeroport();
             switch (choix) {
-                case 1:
+                case 1://ajouter
                     ajoutAeroport();
                     break;
-                case 2:
+                case 2://afficher
                     listeAeroports();
                     break;
-                case 3:
+                case 3://rechercher
                     rechAeroport();
                     break;
-                case 4:
+                case 4://supprimer
                     suppAeroport();
                     break;
-                case 5:
+                case 5://retour
                     ov.affMsg("Retour au menu principal !");
                     ov.menu();
                     break;
@@ -177,24 +198,27 @@ public class OperatorControleur {
         } while (choix != 5);
     }
     
+    /**
+     * Gestion du menu des voyages
+     */
     public void gestionVoyage() {
         int choix;
         do {
             choix = ov.menuVoyage();
             switch (choix) {
-                case 1:
+                case 1://ajouter
                     //ajoutVoyage();
                     break;
-                case 2:
+                case 2://ajouter
                     //ajoutDeplacement();
                     break;
-                case 3:
+                case 3://affilier
                     //OrganiserVoyage();
                     break;
-                case 4:
+                case 4://afficher
                     //ListeVoyages();
                     break;
-                case 5:
+                case 5://retour
                     ov.affMsg("Retour au menu principal !");
                     ov.menu();
                     break;
@@ -204,30 +228,45 @@ public class OperatorControleur {
         } while (choix != 5);
     }
 
+    /**
+     * Méthode qui ajoute un bateau
+     */
     public void ajoutBateau() {
         Bateau b = ov.encodeBateau();
         String msg = om.ajouterBateau(b);
         ov.affMsg(msg);
     }
 
+    /**
+     * méthode qui ajoute un port
+     */
     public void ajoutPort() {
         Port p = ov.encodePort();
         String msg = om.ajouterPort(p);
         ov.affMsg(msg);
     }
 
+    /**
+     * méthode qui ajoute un aéroport
+     */
     public void ajoutAeroport() {
         Aeroport a = ov.encodeAeroport();
         String msg = om.ajouterAeroport(a);
         ov.affMsg(msg);
     }
 
+    /**
+     * méthode qui ajoute un vol
+     */
     public void ajoutVol() {
         Vol v = ov.encodeVol();
         String msg = om.ajouterVol(v);
         ov.affMsg(msg);
     }
 
+    /**
+     * méthode qui associe un port à un bateau
+     */
     public void affilierPort() {
         listeBateaux();
         Bateau b = rechBateau();
@@ -263,6 +302,9 @@ public class OperatorControleur {
         ov.affMsg(msg);
     }
 
+    /**
+     * méthode qui associe un aéroport à un vol
+     */
     public void affilierAeroport() {
         listeVols();
         Bateau b = rechBateau();
@@ -298,6 +340,9 @@ public class OperatorControleur {
         ov.affMsg(msg);
     }
 
+    /**
+     * méthode qui modifie le supplément single d'un bateau
+     */
     public void modifSupSingle() {
         Bateau b = rechBateau();
         if (b == null) {
@@ -311,6 +356,9 @@ public class OperatorControleur {
         ov.affMsg(msg);
     }
 
+    /**
+     * méthode qui supprime un bateau
+     */
     public void suppBateau() {
         Bateau b = rechBateau();
         if (b == null) {
@@ -328,6 +376,9 @@ public class OperatorControleur {
 
     }
 
+    /**
+     * méthode qui supprime un port
+     */
     public void suppPort() {
         Port p = rechPort();
         if (p == null) {
@@ -345,6 +396,9 @@ public class OperatorControleur {
 
     }
 
+    /**
+     * méthode qui supprime un vol
+     */
     public void suppVol() {
         Vol v = rechVol();
         if (v == null) {
@@ -362,6 +416,9 @@ public class OperatorControleur {
 
     }
 
+    /**
+     * méthode qui supprime un aéroport
+     */
     public void suppAeroport() {
         Aeroport a = rechAeroport();
         if (a == null) {
@@ -379,26 +436,45 @@ public class OperatorControleur {
 
     }
 
+    /**
+     * méthode qui recherche un bateau
+     * @return retourne un bateau
+     */
     public Bateau rechBateau() {
         String codeBateau = ov.rechBateau();
         return om.getBateau(codeBateau);
     }
 
+    /**
+     * méthode qui recherche un port
+     * @return retourne un port
+     */
     public Port rechPort() {
         String codePort = ov.rechPort();
         return om.getPort(codePort);
     }
 
+    /**
+     * méthode qui recherche un vol
+     * @return retourne un vol
+     */
     public Vol rechVol() {
         String codeVol = ov.rechVol();
         return om.getVol(codeVol);
     }
 
+    /**
+     * méthode qui recherche un aéroport
+     * @return retourne un aéroport
+     */
     public Aeroport rechAeroport() {
         String codeAeroport = ov.rechAeroport();
         return om.getAeroport(codeAeroport);
     }
 
+    /**
+     * méthode qui affiche la liste des bateaux
+     */
     public void listeBateaux() {
 
         List<Bateau> lb = om.tousBateaux();
@@ -406,6 +482,9 @@ public class OperatorControleur {
 
     }
 
+    /**
+     * méthode qui affiche la liste des ports
+     */
     public void listePorts() {
 
         List<Port> lp = om.tousPorts();
@@ -413,11 +492,17 @@ public class OperatorControleur {
 
     }
 
+    /**
+     * méthode qui affiche la liste des vols
+     */
     public void listeVols() {
         List<Vol> lv = om.tousVols();
         ov.affListe(lv);
     }
 
+    /**
+     * méthode qui affiche la liste des aéroports
+     */
     public void listeAeroports() {
         List<Aeroport> la = om.tousAeroports();
         ov.affListe(la);
